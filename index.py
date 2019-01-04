328590530be5513fccb36d645e7148e09b7580fb
一一秋风 <491883290@qq.com>
2019-01-04 10:12:08 +0800

    @

---
+++ index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+def quadratic(a,b,c)
+  if not isinstance(a,(int,float))
+  	raise TypeError('a is not number')
+  if not isinstance(b,(int,float))
+  	raise TypeError('b is not a number')
+  if not isinstance(c,(int,float))
+  	raise TypeError('c is not a number')
+  d=
