8dd6a2e79669477933b0e5813c8ec94c6b2fea76
一一秋风 <491883290@qq.com>
2019-02-23 17:57:58 +0800

    @

--- flask-webapp/index.py
+++ flask-webapp/index.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask import request
+from flask import send_from_directory,redirect, url_for
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = './images/aaa'
+
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -20,6 +27,21 @@ def login():
     error = None
     if request.method == 'POST':
       return request.args.get('aaaa', '111')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['fileupload']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print("path:%s"  % os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file',filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 
 if __name__=='__main__':
   app.debug=True
